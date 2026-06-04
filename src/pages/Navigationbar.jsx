02e18beeb0ffbaf0b9a396a3d86eb3015003f9b4
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2026-06-04 13:06:03 +0200

    Navigation

--- src/pages/Navigationbar.jsx
+++ src/pages/Navigationbar.jsx
@@ -4,10 +4,10 @@ const Navigationbar = () => {
     return (
         <>
 
-            <nav>
-                <ul>
-                    <li><Link to="/documentatie">Documentatie</Link></li>
-                    <li><Link to="/downloaden">Downloaden</Link></li>
+            <nav className="navigation">
+                <ul className="unordered-list">
+                    <li className="list-item-documentatie"><Link to="/documentatie">Documentatie</Link></li>
+                    <li className="list-item-downloaden"><Link to="/downloaden">Downloaden</Link></li>
                 </ul>
             </nav >
 
