81fdb155a9d1ee4ec5d234063af2d32aac99c3b9
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2026-06-08 10:18:58 +0200

    commit

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,11 +1,12 @@
 const NoPage = () => {
     return (
 
-
+        <div className="nopage-wrapper">
             <div className="nopage-container">
                 <h1>Pagina niet gevonden!</h1>
                 <p>Sorry, deze pagina bestaat niet.</p>
             </div>
+        </div >
     );
 }
 
