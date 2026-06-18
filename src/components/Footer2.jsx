609d8c119f79fd8b762123b24cac7c75ee67081e
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2026-06-18 12:13:15 +0200

    commit

---
+++ src/components/Footer2.jsx
@@ -0,0 +1,14 @@
+const Footer2 = () => {
+    return (
+        <>
+            <footer className="footer-2">
+                <div className="footer-container-2">
+                    <img src="/src/assets/node.png" alt="" />
+                    <p>Gepresenteerd door Berkan Altinkaya & Lukasz van den Blink</p>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer2;
